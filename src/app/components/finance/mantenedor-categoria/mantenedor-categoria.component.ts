9d57827fddee3e5b3c537b8b8cc0eff933be6fb0
Omar Guerra Alfaro <omarrrrignacio@gmail.com>
2023-03-21 17:42:12 -0300

    avance...

--- src/app/components/finance/mantenedor-categoria/mantenedor-categoria.component.ts
+++ src/app/components/finance/mantenedor-categoria/mantenedor-categoria.component.ts
@@ -17,6 +17,9 @@ export class MantenedorCategoriaComponent implements OnInit {
   infoActivo: any;
   infoActivo2: any;
   infoActivo3: any;
+  infoActivo4: any;
+  info: any;
+  info2: any;
 
   infoPasivo: any;
   infoPatrimonio: any;
@@ -28,7 +31,7 @@ export class MantenedorCategoriaComponent implements OnInit {
   ngOnInit(): void {
     this.data.getCategory().subscribe((data) => {
       // console.log(Object.entries(data));
-      // console.log(Object.values(data));   
+      // console.log(Object.values(data));
 
       this.infoActivo = data.activo;
       // console.log(this.infoActivo);
@@ -42,13 +45,30 @@ export class MantenedorCategoriaComponent implements OnInit {
         this.infoActivo2.flatMap((item: any) => {
           // console.log(item);
           const activos = Object.values(item);
-          console.log(activos);
+          // console.log(activos);
           activos.map((item: any) => {
             // console.log(item);
             this.infoActivo3 = item;
           });
-          console.log(this.infoActivo3);
-          
+          // console.log(this.infoActivo3);
+
+          this.infoActivo3.filter((item: any) => {
+            // console.log(item);
+
+            this.infoActivo4 = Object.values(item);
+            console.log(this.infoActivo4);
+          });
+
+          // this.infoActivo3.map((item: any) => {
+          //   console.log(item)
+          //   this.info = Object.values(item);
+          //   const [cod, nombre_cod] = this.info;
+          //   this.info2 = [
+          //     cod,
+          //     nombre_cod
+          //   ];
+          // });
+
 
         });
 
