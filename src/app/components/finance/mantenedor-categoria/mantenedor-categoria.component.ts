2d77817199b4714780a28c202456a5ad3ab7f210
Omar Guerra Alfaro <omarrrrignacio@gmail.com>
2023-03-21 14:04:52 -0300

    mantenedor

--- src/app/components/finance/mantenedor-categoria/mantenedor-categoria.component.ts
+++ src/app/components/finance/mantenedor-categoria/mantenedor-categoria.component.ts
@@ -12,8 +12,12 @@ export class MantenedorCategoriaComponent implements OnInit {
 
 
   infoCategorias: any[] = [];
+
+
   infoActivo: any;
   infoActivo2: any;
+  infoActivo3: any;
+
   infoPasivo: any;
   infoPatrimonio: any;
   infoIngreso: any;
@@ -22,31 +26,34 @@ export class MantenedorCategoriaComponent implements OnInit {
   constructor(private data: DataCategoryService) { }
 
   ngOnInit(): void {
-
-
     this.data.getCategory().subscribe((data) => {
       // console.log(Object.entries(data));
       // console.log(Object.values(data));   
 
-
       this.infoActivo = data.activo;
-      console.log(this.infoActivo);
+      // console.log(this.infoActivo);
 
       this.infoActivo.map((item: any) => {
         this.infoCategorias.push(item);
         this.infoCategorias.filter((item: any) => {
-          console.log(item.activo_circulante);
+          // console.log(item.activo_circulante);
           this.infoActivo2 = item.activo_circulante;
         });
+        this.infoActivo2.flatMap((item: any) => {
+          // console.log(item);
+          const activos = Object.values(item);
+          console.log(activos);
+          activos.map((item: any) => {
+            // console.log(item);
+            this.infoActivo3 = item;
+          });
+          console.log(this.infoActivo3);
+          
+
+        });
+
       });
-      
-
-      
-
-
-
     });
 
   }
-
 }
